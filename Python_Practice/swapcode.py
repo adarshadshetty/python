3bc87d4b4826d79e571498dbdc72e928cb44d353
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-23 17:13:28 +0530

    Update swapcode.py

--- Python_Practice/swapcode.py
+++ Python_Practice/swapcode.py
@@ -6,3 +6,22 @@ def swapcode(newList):
     return newList
 newList = [5,25,50,100,105 ]
 print(swapcode(newList))
+
+
+
+#  Different approach
+def swapcode_v1(newListv1):
+    newListv1[0] ,  newListv1[-1] = newListv1[-1], newListv1[0]
+    return newListv1
+newListv1= [5,25,50,100,105 ]
+print(swapcode_v1(newListv1))
+
+
+
+# Different approach
+def swapcode_v2(newListv2):
+    get= newListv2[-1], newListv2[0]
+    newListv2[0],newListv2[-1]=get 
+    return newListv2
+newListv2= [5,25,50,100,105 ]
+print(swapcode_v2(newListv2))
